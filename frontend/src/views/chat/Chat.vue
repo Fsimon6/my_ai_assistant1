64428232b359606f1421f8e1115139f1f286b71e
Fsimon6 <2376709678@qq.com>
2026-09-17 17:07:35 +0800

    fix: make knowledge base navigation and chat history reliable

--- frontend/src/views/chat/Chat.vue
+++ frontend/src/views/chat/Chat.vue
@@ -31,6 +31,12 @@
           </el-icon>
           导出
         </el-button>
+        <el-button type="text" @click="showKnowledgeBase">
+          <el-icon>
+            <Folder/>
+          </el-icon>
+          知识库
+        </el-button>
         <el-dropdown @command="handleToolCommand">
           <el-button type="text">
             <el-icon>
